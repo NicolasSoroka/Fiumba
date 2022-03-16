e3b821e99e48782137decaced948ff405eb4057d
Nicolas Soroka <sorokanicolas@gmail.com>
2022-03-16 00:18:46 -0300

    test

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,16 +1,17 @@
 import Head from 'next/head';
 import styles from './Layout.module.css';
 import { Header, Footer } from '../index';
+import { motion } from 'framer-motion';
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.layout}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>Fiumba</title>
       </Head>
       <Header/>
-      <main>{children}</main>
+      <motion.main className={ styles.layout__main }>{children}</motion.main>
       <Footer/>
     </div>
   )
