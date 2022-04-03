f56960e72e1d40e40b222e697a28919236668bf4
Nicolas Soroka <sorokanicolas@gmail.com>
2022-04-03 20:41:23 -0300

    k

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -9,14 +9,14 @@ const Layout = ({ children }) => {
       <Head>
         <style>
           @import
-          url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;1,200;1,300;1,400;1,600&display=swap');
+          url(&quot;https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;1,200;1,300;1,400;1,600&display=swap&quot;);
         </style>
         <link rel="icon" href="/favicon.ico" />
         <title>Fiumba</title>
       </Head>
       <Header />
-      <FloatingNav />
       <motion.main>{children}</motion.main>
+      <FloatingNav />
       <Footer />
     </div>
   );
